aabb0638c124dd5e527adf371bb50d6966a983a0
curbaniATU <curbani@atu.edu>
2025-04-19 00:04:03 -0500

    Server.js Setup: Implemented Express.js with simple GET route

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
