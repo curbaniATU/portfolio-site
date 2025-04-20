60c607c62a3edd41abeb4e5c80fa92581c897a34
curbaniATU <curbani@atu.edu>
2025-04-19 20:07:48 -0500

    Router Setup: Initialized routes in index.js

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+router.get('/', mainController.home);
+router.get('/contact', mainController.contact);
+router.get('/education', mainController.education);
+router.get('/projects', mainController.projects);
+router.get('/skills', mainController.skills);
+
+module.exports = router;
