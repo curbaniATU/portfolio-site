ddfc874fbd7cf361f7e26e02cab16d9855289bdf
curbaniATU <curbani@atu.edu>
2025-04-19 20:09:07 -0500

    Router Setup: Initialize router in server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/', indexRouter);
 
 app.set('view engine', 'ejs');
 
